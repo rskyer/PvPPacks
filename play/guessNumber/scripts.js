9d950cb91fb7266d3c589758e29791590df14e2a
skyer <s786.mc.yt@gmail.com>
2026-08-08 15:03:09 +0330

    Update scripts.js
    Adding SOUNDS

--- play/guessNumber/scripts.js
+++ play/guessNumber/scripts.js
@@ -4,7 +4,19 @@ let userGuess = ""
 
 let numArea = document.getElementById("numArea")
 let beOnArea = ""
-
+const clickSound = new Audio("./sounds/click.ogg");
+const click2Sound = new Audio("./sounds/click2.wav");
+const winSound = new Audio("./sounds/win.ogg");
+const loseSound = new Audio("./sounds/lose.ogg");
+const reloadSound = new Audio("./sounds/reload.ogg");
+function playSound(name) {
+    try {
+        name.play();
+    } catch(err) {
+        console.log("Cannot find" + name)
+    }
+    
+}
 function updateArea(wh) {
     beOnArea += wh
     
@@ -14,7 +26,9 @@ function updateArea(wh) {
             beOnArea = ""
         }, 1000)
     }
+    
     numArea.textContent = beOnArea  // load 
+    playSound(clickSound)
 }
 
 function clickedZero() {
@@ -50,30 +64,43 @@ function clickedNine() {
 function clickedClear() {
     beOnArea = ""
     numArea.textContent = ""
+    playSound(click2Sound)
 }
 function clickedRemoveLast() {
     beOnAreaCH = beOnArea.length - 1
     beOnArea = beOnArea.slice(0, -1)
     numArea.textContent = beOnArea + ""
+    playSound(click2Sound)
 }
 let mode = ""
 let chances = 10
 var isPlayerWin = false;
 var isPlayerLose = false;
+let hintArea = document.getElementById("hintArea")
 function checkUserInput() {
     chances -= 1
     let beOnAreaint = parseInt(beOnArea)
     if(chances < 0) {
         isPlayerLose = true
+        hintArea.style.color = "red"
+        document.body.style.backgroundImage =
+    "linear-gradient(45deg, white, red)";
+        
         mode = "متاستفم ولی باختی:/"
+        title.textContent = "باخت بد!"
+        setTimeout(function() {
+            title.style.color = "black"
+        }, 1000)
         
         document.getElementById("hintArea").textContent = mode
+        playSound(loseSound)
         return
         
     } else if(beOnAreaint == thatNumber) {
-        
+        hintArea.style.color = "skyblue"
         mode = "آفرین بردی! عدد " + thatNumber + " بود."
         isPlayerWin = true;
+        playSound(winSound)
         
     } else if (beOnAreaint < thatNumber) {
         mode = "بیشتر از " + beOnArea + " تاست! فقط " + chances + " تا شانس داری"
@@ -87,9 +114,12 @@ function checkUserInput() {
         
     }
     document.getElementById("hintArea").textContent = mode
+    playSound(click2Sound)
 }
 function resetGame() {
+    playSound(reloadSound)
     location.reload()  // reload page func
+    
 }
 let title = document.getElementById("title")
 function clickedResetPage() {

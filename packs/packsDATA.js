837a12710f7646e753cf9dfd11cab0d2086ce95a
skyer <s786.mc.yt@gmail.com>
2026-08-16 14:25:49 +0330

    Create packsDATA.js

---
+++ packs/packsDATA.js
@@ -0,0 +1,9 @@
+const packsData = {
+    Skyer: {
+        name: "Skyer",
+        description: "پک اسکایر، پک خودم ، مناسب رنکد بدوارز و ۳۲ ایکس",
+        downloadLink: "https://exam.io/div.zip",
+        imageURL: "https://rskyer.io/div.png",
+        version: "all"
+    }
+};

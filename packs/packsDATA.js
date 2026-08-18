2be9d3002db15a9765ac7c849842c2d966fa16e7
skyer <s786.mc.yt@gmail.com>
2026-08-18 13:02:53 +0330

    Update packsDATA.js

--- packs/packsDATA.js
+++ packs/packsDATA.js
@@ -5,5 +5,12 @@ const packsData = {
         downloadLink: "https://exam.io/div.zip",
         imageURL: "https://rskyer.io/div.png",
         version: "all"
-    }
+    },
+  Sky: {
+      name: "Sky",
+      description: "پک فوق العاده زیبا مخصوص ماینکرافت بدراک",
+      downloadLink: "https://www.mediafire.com/file/yx7k8462jof2i00/SKY.mcpack/file",
+      imageURL: "https://.png",
+      version: "ALL"
+  }
 };

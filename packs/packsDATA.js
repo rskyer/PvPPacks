0e4f17b3da244b2b376abfdf6ab8e8279467d453
skyer <s786.mc.yt@gmail.com>
2026-08-18 13:12:59 +0330

    Update packsDATA.js

--- packs/packsDATA.js
+++ packs/packsDATA.js
@@ -4,13 +4,13 @@ const packsData = {
         description: "پک اسکایر، پک خودم ، مناسب رنکد بدوارز و ۳۲ ایکس",
         downloadLink: "https://exam.io/div.zip",
         imageURL: "https://rskyer.io/div.png",
-        version: "all"
+        version: "ALL"
     },
-  Sky: {
-      name: "Sky",
-      description: "پک فوق العاده زیبا مخصوص ماینکرافت بدراک",
-      downloadLink: "https://www.mediafire.com/file/yx7k8462jof2i00/SKY.mcpack/file",
-      imageURL: "https://.png",
-      version: "ALL"
-  }
+    Sky: {
+        name: "Sky",
+        description: "پک فوق العاده زیبا مخصوص ماینکرافت بدراک",
+        downloadLink: "https://www.mediafire.com/file/yx7k8462jof2i00/SKY.mcpack/file",
+        imageURL: "https://example.com/sky-thumbnail.png",
+        version: "ALL"
+    }
 };

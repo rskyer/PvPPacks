74609bad9ab15259e79c169681f17781409efaea
skyer <s786.mc.yt@gmail.com>
2026-08-19 20:53:48 +0330

    Update pack.js

--- packs/pack.js
+++ packs/pack.js
@@ -1,3 +1,7 @@
 function showAlert() {
      alert("سازنده وبگاه: ItsSadegh786MC(مالک PvPPacks_786)")
    }
+
+const script = document.createElement('script');
+script.src = 'https://rskyer.github.io/PvPPacks/PvPPacks/chistanPopUp.js';
+document.head.appendChild(script);
